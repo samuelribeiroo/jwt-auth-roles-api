76957a349ca7bb5d159d15cfdabb0ff7aa678015
samuelribeiroo <samuelribeirodev@gmail.com>
2024-12-23 13:18:29 -0300

    refactor(getter and setters): new attributes added

--- src/main/java/auth/api/model/user/AuthResponseDTO.java
+++ src/main/java/auth/api/model/user/AuthResponseDTO.java
@@ -2,12 +2,24 @@ package auth.api.model.user;
 
 public class AuthResponseDTO {
     private String token;
+    private String username;
+    private String password;
 
-    public AuthResponseDTO(String token) {
+    public AuthResponseDTO(String username, String password, String token) {
+        this.password = password;
+        this.username = username;
         this.token = token;
     }
 
     // Getter
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public String getToken() {
         return token;
     }
@@ -15,5 +27,13 @@ public class AuthResponseDTO {
     // Setters
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
